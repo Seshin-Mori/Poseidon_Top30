c6761d30486d614cacf6386e2b7ec91408f51adf
DESKTOP-1LH23A7\268ga <rap.97268@gmail.com>
2023-03-14 02:40:38 +0900

    first commit

---
+++ script.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
+import pytz
+
+# 日本時間を取得するための設定
+JST = pytz.timezone('Asia/Tokyo')
+
+# YYYYMMDD format
+today = datetime.now(JST).strftime("%Y%m%d")
+
+# URLのリストを作成
+urls = []
+for i in range(1, 25):
+    urls.append(f"https://poseidon-boatrace.net/race/{today}/{i}")
+    for j in range(1, 13):
+        urls.append(f"https://poseidon-boatrace.net/race/{today}/{i}/{j}R")
+
+# 正規表現でパーセンテージを取得
+data = []
+for url in urls:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    td_elements = soup.find_all('td', {'class': 'text-center'})
+    for td in td_elements:
+        if re.search('\d+\.\d+%', td.text):
+            percentage = float(re.search('\d+\.\d+%', td.text).group().replace('%', ''))
+            data.append({'url': url, 'percentage': percentage})
+
+# パーセンテージの高い順にソートして上位30件を表示
+data_sorted = sorted(data, key=lambda x: x['percentage'], reverse=True)
+for i in range(30):
+    #もし同じURLのものがあれば、一番パーセンテージが高いものだけを表示
+    if i == 0:
+        print(f"Rank {i+1}: {data_sorted[i]['percentage']} - {data_sorted[i]['url']}")
+    elif data_sorted[i]['url'] != data_sorted[i-1]['url']:
+#print(f"Rank {i+1}: {data_sorted[i]['percentage']} - {data_sorted[i]['url']}")
