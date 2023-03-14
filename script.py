6bb92742bb6d1ae5a7f3fbddc3e2ab5c4b9edd35
DESKTOP-1LH23A7\268ga <rap.97268@gmail.com>
2023-03-15 00:49:15 +0900

    一旦完成

--- script.py
+++ script.py
@@ -16,33 +16,47 @@ for i in range(1, 25):
     urls.append(f"https://poseidon-boatrace.net/race/{today}/{i}")
     for j in range(1, 13):
         urls.append(f"https://poseidon-boatrace.net/race/{today}/{i}/{j}R")
-        
-data = []
 
+# 正規表現でパーセンテージを取得
+data = []
 for url in urls:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     td_elements = soup.find_all('td', {'class': 'text-center'})
     for td in td_elements:
-        td_text = td.text
-        if re.search('\d+\.\d+%', td_text):
-            percentage = float(re.search('\d+\.\d+%', td_text).group().replace('%', ''))
-            odds, time = None, None
-            for inner_td in td.parent.find_all('td', {'class': 'text-center'}):
-                inner_td_text = inner_td.text
-                if odds is None and re.search('\d+\.\d+', inner_td_text):
-                    odds = float(re.search('\d+\.\d+', inner_td_text).group())
+        if re.search('\d+\.\d+%', td.text):
+            percentage = float(re.search('\d+\.\d+%', td.text).group().replace('%', ''))
+            data.append({'url': url, 'percentage': percentage})
+#X.Xをオッズとして取得。---の場合---を入れる
+for url in urls:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    td_elements = soup.find_all('td', {'class': 'text-center'})
+    for td in td_elements:
+        if re.search('\d+\.\d+', td.text):
+            odds = float(re.search('\d+\.\d+', td.text).group())
+            for d in data:
+                if d['url'] == url:
+                    d['odds'] = odds
+        elif re.search('---', td.text):
+            for d in data:
+                if d['url'] == url:
+                    d['odds'] = '---'
+#urlのページからli属性（クラス名なし）を取得、[hh:mm]を取得
+for url in urls:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    li_elements = soup.find_all('li')
+    for li in li_elements:
+        if re.search('\[\d+:\d+\]', li.text):
+            time = re.search('\[\d+:\d+\]', li.text).group()
+            for d in data:
+                if d['url'] == url:
+                    d['time'] = time
 
-#ページ全体のli要素から[hh:mm]を取得し、timeに代入。これはtext-centerクラスのtd要素の中にはないため、ページ全体のli要素から取得する必要がある。
-for li in soup.find_all('li'):
-    li_text = li.text
-    if re.search('\[\d+:\d+\]', li_text):
-        time = re.search('\[\d+:\d+\]', li_text).group().replace('[', '').replace(']', '')
-        break
-    data.append({'percentage': percentage, 'odds': odds, 'time': time, 'url': url})
-
-data_sorted = sorted([d for d in data if d['percentage'] >= 50], key=lambda x: x['percentage'], reverse=True)
-for d in data_sorted[:30]:
-    print(f"{d['percentage']}% {d['odds']} {d['time']} {d['url']}")
-        
+# パーセンテージの高い順にソートして上位30件を表示50%以上のものを表示
+data.sort(key=lambda x: x['percentage'], reverse=True)
+for d in data:
+    if d['percentage'] >= 50:
+        print(d)
 
