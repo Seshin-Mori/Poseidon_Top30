335c762dcb1da0675655fa7ad366ebe8fd42e88d
DESKTOP-1LH23A7\268ga <rap.97268@gmail.com>
2023-03-14 23:01:17 +0900

    オッズと時間を抽出できるように修正

--- script.py
+++ script.py
@@ -16,23 +16,29 @@ for i in range(1, 25):
     urls.append(f"https://poseidon-boatrace.net/race/{today}/{i}")
     for j in range(1, 13):
         urls.append(f"https://poseidon-boatrace.net/race/{today}/{i}/{j}R")
-
-# 正規表現でパーセンテージを取得
+        
 data = []
+
 for url in urls:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     td_elements = soup.find_all('td', {'class': 'text-center'})
     for td in td_elements:
-        if re.search('\d+\.\d+%', td.text):
-            percentage = float(re.search('\d+\.\d+%', td.text).group().replace('%', ''))
-            data.append({'url': url, 'percentage': percentage})
+        td_text = td.text
+        if re.search('\d+\.\d+%', td_text):
+            percentage = float(re.search('\d+\.\d+%', td_text).group().replace('%', ''))
+            odds, time = None, None
+            for inner_td in td.parent.find_all('td', {'class': 'text-center'}):
+                inner_td_text = inner_td.text
+                if odds is None and re.search('\d+\.\d+', inner_td_text):
+                    odds = float(re.search('\d+\.\d+', inner_td_text).group())
+                #li要素から[hh:mm]を取得。td要素の外側にあるため、parentを使う
+                if time is None and re.search('\[\d+:\d+\]', inner_td.parent.text):
+                    time = re.search('\[\d+:\d+\]', inner_td.parent.text).group()
+            data.append({'percentage': percentage, 'odds': odds, 'time': time, 'url': url})
 
-# パーセンテージの高い順にソートして上位30件を表示
-data_sorted = sorted(data, key=lambda x: x['percentage'], reverse=True)
-for i in range(30):
-    #50%以上のものだけ表示
-    if data_sorted[i]['percentage'] >= 50:
-        #見やすいフォーマットに整形
-        print(f"{data_sorted[i]['url']} {data_sorted[i]['percentage']}%")
+data_sorted = sorted([d for d in data if d['percentage'] >= 50], key=lambda x: x['percentage'], reverse=True)
+for d in data_sorted[:30]:
+    print(f"{d['percentage']}% {d['odds']} {d['time']} {d['url']}")
+        
 
