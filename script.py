bf49c81fe81a857a949f077b2e81dc3f229f0c76
DESKTOP-1LH23A7\268ga <rap.97268@gmail.com>
2023-03-15 01:15:55 +0900

    時間順ソートにして仕上げ処理をして完成

--- script.py
+++ script.py
@@ -19,44 +19,53 @@ for i in range(1, 25):
 
 # 正規表現でパーセンテージを取得
 data = []
+
+# リストの中身をループして、パーセンテージ、オッズ、時間を取得する。
 for url in urls:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     td_elements = soup.find_all('td', {'class': 'text-center'})
+
     for td in td_elements:
-        if re.search('\d+\.\d+%', td.text):
-            percentage = float(re.search('\d+\.\d+%', td.text).group().replace('%', ''))
-            data.append({'url': url, 'percentage': percentage})
-#X.Xをオッズとして取得。---の場合---を入れる
-for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    td_elements = soup.find_all('td', {'class': 'text-center'})
-    for td in td_elements:
-        if re.search('\d+\.\d+', td.text):
-            odds = float(re.search('\d+\.\d+', td.text).group())
+        td_text = td.text
+        if re.search('\d+\.\d+%', td_text):
+            percentage = float(re.search('\d+\.\d+%', td_text).group()[:-1])
+            item = {'url': url, 'percentage': percentage}
+            data.append(item)
+
+        elif re.search('\d+\.\d+', td_text):
+            odds = float(re.search('\d+\.\d+', td_text).group())
             for d in data:
                 if d['url'] == url:
                     d['odds'] = odds
-        elif re.search('---', td.text):
+
+        elif td_text == '---':
             for d in data:
                 if d['url'] == url:
-                    d['odds'] = '---'
-#urlのページからli属性（クラス名なし）を取得、[hh:mm]を取得
-for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+                    d['odds'] = td_text
+# [hh:mm]の時間を取得する。
     li_elements = soup.find_all('li')
     for li in li_elements:
-        if re.search('\[\d+:\d+\]', li.text):
-            time = re.search('\[\d+:\d+\]', li.text).group()
+        li_text = li.text
+        if re.search('\[\d+:\d+\]', li_text):
+            time = re.search('\[\d+:\d+\]', li_text).group()
             for d in data:
                 if d['url'] == url:
                     d['time'] = time
 
-# パーセンテージの高い順にソートして上位30件を表示50%以上のものを表示
-data.sort(key=lambda x: x['percentage'], reverse=True)
+# Sort and print
+#data = sorted(filter(lambda x: x['percentage'] >= 50, data), key=lambda x: x['percentage'], reverse=True)
+#[hh:mm]の時間が早い順に配列をソートする。
+data = sorted(data, key=lambda x: x['time'])
+
+#フォーマットを見やすくするために、リストの中身を整形して要素を区切り表のようにする。オッズが2.0以上の場合は、赤字で表示する。
 for d in data:
-    if d['percentage'] >= 50:
-        print(d)
+    if d['odds'] == '---':
+        d['odds'] = '---'
+    elif d['odds'] >= 2.0:
+        d['odds'] = f"\033[31m{d['odds']}\033[0m"
+    else:
+        d['odds'] = f"{d['odds']}"
+    print(f"{d['url']} {d['percentage']}% {d['odds']} {d['time']}")
+
 
